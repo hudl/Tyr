0b6dce6d001dc567cf7d5532a2304cd5ac63996c
Mihir Singh <git.service@mihirsingh.com>
2015-02-13 11:11:31 -0600

    Defined a logger to provide feedback to the user

--- servers/server.py
+++ servers/server.py
@@ -1,8 +1,19 @@
 from exceptions import *
 import boto.ec2
+import logging
 import sys
 
 class Server(object):
+
+    log = logging.getLogger('Servers.Server')
+    log.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+            '%(asctime)s [%(name)s] %(levelname)s: %(message)s',
+            datefmt='%H:%M:%S')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
 
     def __init__(self, dry, verbose, size, cluster, environment, ami,
                     region, role, keypair, availability_zone):
