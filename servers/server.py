06f16a935f8d155131fda5c39f1d0a8a6b72899c
Mihir Singh <git.service@mihirsingh.com>
2015-02-13 09:56:18 -0600

    Created a servers package with a server module with a Server class

---
+++ servers/server.py
@@ -0,0 +1,19 @@
+import boto.ec2
+import sys
+
+class Server(object):
+
+    def __init__(self, dry, verbose, size, cluster, environment, ami,
+                    region, role, keypair, availability_zone):
+
+        self.dry = dry
+        self.verbose = verbose
+        self.size = size
+        self.cluster = cluster
+        self.environment = environment
+        self.ami = ami
+        self.region = region
+        self.role = role
+        self.keypair = keypair
+        self.availability_zone = availability_zone
+
