16fe7d9796e996ba38783a5e40e8b46eaf5327d5
Mihir Singh <git.service@mihirsingh.com>
2015-02-25 15:48:07 -0600

    Setup the logger for the MongoDB arbiter node

--- servers/mongo/arbiter.py
+++ servers/mongo/arbiter.py
@@ -2,4 +2,12 @@ from .. import Server
 
 class MongoArbiterNode(Server):
 
-    pass
+    log = logging.getLogger('Servers.MongoArbiterNode')
+    log.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+            '%(asctime)s [%(name)s] %(levelname)s: %(message)s',
+            datefmt = '%H:%M:%S')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
