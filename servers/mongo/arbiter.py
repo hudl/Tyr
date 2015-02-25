e4a1b8f7ce34403efcd046fd3081f6ce175418fc
Mihir Singh <git.service@mihirsingh.com>
2015-02-25 15:51:36 -0600

    Defined the __init__ method for the arbiter node

--- servers/mongo/arbiter.py
+++ servers/mongo/arbiter.py
@@ -11,3 +11,18 @@ class MongoArbiterNode(Server):
             datefmt = '%H:%M:%S')
     ch.setFormatter(formatter)
     log.addHandler(ch)
+
+    def __init__(self, dry = None, verbose = None, size = None, cluster = None,
+                    environment = None, ami = None, region = None, role = None,
+                    keypair = None, availability_zone = None, chef_path = None,
+                    security_groups = None, block_devices = None,
+                    replica_set = None)
+
+        super(MongoArbiterNode, self).__init__(dry, verbose, size, cluster,
+                                                environment, ami, region, role,
+                                                keypair, availability_zone,
+                                                security_groups, block_devices)
+
+        self.replica_set = replica_set
+        self.chef_path = chef_path
+
