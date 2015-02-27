819285e2d3bfd2c12d235e742b71aa847dafd8e0
Mihir Singh <git.service@mihirsingh.com>
2015-02-27 12:23:36 -0600

    Added a setup.py to install the Tyr package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'tyr',
+    version = '0.0.1',
+    author = 'Mihir Singh (@citruspi)',
+    author_email = 'mihir.singh@hudl.com',
+    packages = ['tyr'],
+    zip_safe = False,
+    include_package_date = True,
+    platforms = 'any',
+    install_requires = [
+        'boto',
+        'pyChef',
+    ]
+)
