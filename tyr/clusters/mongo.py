4f189c20fc535f780e905e6fee7351329240d8fc
Mihir Singh <git.service@mihirsingh.com>
2015-03-02 10:15:25 -0600

    Set up logging for a MongoDB cluster

---
+++ tyr/clusters/mongo.py
@@ -0,0 +1,14 @@
+import logging
+
+class MongoCluster(object):
+
+    log = logging.getLogger('Clusters.Mongo')
+    log.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+            '%(asctime)s [%(name)s] %(levelname)s: %(message)s',
+            datefmt = '%H:%M:%S')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
