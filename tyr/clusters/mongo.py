366838ede54a9323abca93b87b71e4e12c6cac46
Mihir Singh <git.service@mihirsingh.com>
2015-03-02 10:20:38 -0600

    Defined the __init__ for the MongoDB cluster

--- tyr/clusters/mongo.py
+++ tyr/clusters/mongo.py
@@ -12,3 +12,25 @@ class MongoCluster(object):
     ch.setFormatter(formatter)
     log.addHandler(ch)
 
+    def __init__(self, dry = None, verbose = None, size = None, cluster = None,
+                    environment = None, ami = None, region = None, role = None,
+                    keypair = None, chef_path = None, replica_set = None,
+                    security_groups = None, block_devices = None,
+                    data_nodes=None):
+
+        self.nodes = []
+
+        self.dry = dry
+        self.verbose = verbose
+        self.size = size
+        self.cluster = cluster
+        self.environment = environment
+        self.ami = ami
+        self.region = region
+        self.role = role
+        self.keypair = keypair
+        self.chef_path = chef_path
+        self.security_groups = security_groups
+        self.block_devices = block_devices
+        self.replica_set = replica_set
+        self.data_nodes = data_nodes
