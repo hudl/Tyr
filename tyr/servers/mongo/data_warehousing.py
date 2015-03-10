6441afb6c3949af5a0da1aa7fde129418f27b795
Mihir Singh <git.service@mihirsingh.com>
2015-03-10 11:04:17 -0500

    Defined __init__() for the Mongo Data Warehousing Node

--- tyr/servers/mongo/data_warehousing.py
+++ tyr/servers/mongo/data_warehousing.py
@@ -12,3 +12,23 @@ class MongoDataWarehousingNode(Server):
             datefmt = '%H:%M:%S')
     ch.setFormatter(formatter)
     log.addHandler(ch)
+
+    def __init__(self, dry = None, verbose = None, size = None, cluster = None,
+                    environment = None, ami = None, region = None, role = None,
+                    keypair = None, availability_zone = None, chef_path = None,
+                    security_groups = None, block_devices = None,
+                    replica_set = None, data_volume_size=None,
+                    data_volume_iops=None, role_policies=None):
+
+        super(MongoDataNode, self).__init__(dry, verbose, size, cluster,
+                                            environment, ami, region, role,
+                                            keypair, availability_zone,
+                                            security_groups, block_devices,
+                                            role_policies)
+
+        self.replica_set = replica_set
+        self.chef_path = chef_path
+        self.data_volume_size = data_volume_size
+        self.data_volume_iops = data_volume_iops
+
+
