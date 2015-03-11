125b181008cbf39126e2439cdaca293ac3333f81
Mihir Singh <git.service@mihirsingh.com>
2015-03-11 13:36:12 -0500

    Added a method to the Server class that creates a logger

--- tyr/servers/server.py
+++ tyr/servers/server.py
@@ -42,6 +42,25 @@ class Server(object):
         self.chef_path = chef_path
         self.role_policies = role_policies
 
+    def establish_logger(self):
+
+        try:
+            return self.log
+        except:
+            pass
+
+        log = logging.getLogger(self.__class__.__name__)
+        log.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+                '%(asctime)s [%(name)s] %(levelname)s: %(message)s',
+                datefmt='%H:%M:%S')
+        ch.setFormatter(formatter)
+        log.addHandler(ch)
+
+        self.log = log
+
     def configure(self):
 
         if self.dry is None:
