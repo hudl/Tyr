4a2ba6a11e0203d1d2505dc74d38b231ea10caad
Mihir Singh <git.service@mihirsingh.com>
2015-03-12 10:50:35 -0500

    Defined the run_mongo method on the Mongo Node

--- tyr/servers/mongo/node.py
+++ tyr/servers/mongo/node.py
@@ -10,10 +10,20 @@ class MongoNode(Server):
                     security_groups = None, block_devices = None,
                     role_policies=None):
 
-        super(MongoArbiterNode, self).__init__(dry, verbose, size, cluster,
+        super(MongoNode, self).__init__(dry, verbose, size, cluster,
                                                 environment, ami, region, role,
                                                 keypair, availability_zone,
                                                 security_groups, block_devices,
                                                 role_policies)
+
+    def run_mongo(self, command):
+
+        template = 'mongo --port 27018 --eval "JSON.stringify({command})"'
+
+        command = template.format(command = command)
+
+        r = self.run(command)
+
+        return json.loads(r['out'].split('\n')[2])
 
 
