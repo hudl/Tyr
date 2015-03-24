8b5be989003fcf919575d6d98583f66f8a236e88
Mihir Singh <git.service@mihirsingh.com>
2015-03-24 10:09:10 -0500

    Changed the default volume iops value to 2000

--- tyr/utilities/replace_mongo_server.py
+++ tyr/utilities/replace_mongo_server.py
@@ -468,7 +468,7 @@ def terminate_decommissioned_node(address, interactive):
 def replace_server(environment = 'test', group = 'monolith',
                     instance_type = 'm3.medium', availability_zone = 'c',
                     replica_set = 1, data_volume_size = 400,
-                    data_volume_iops = 200, mongodb_package_version = '2.4.13',
+                    data_volume_iops = 2000, mongodb_package_version = '2.4.13',
                     member = None, replace = False, node_type = 'data',
                     interactive = True):
 
