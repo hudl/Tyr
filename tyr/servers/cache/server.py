2d687b0f8daacd92372e1b37d56a36272b3e1d5c
Mihir Singh <git.service@mihirsingh.com>
2015-04-20 18:05:33 -0500

    Added the couchbase version, username, and password as arguments

--- tyr/servers/cache/server.py
+++ tyr/servers/cache/server.py
@@ -9,9 +9,14 @@ class CacheServer(Server):
     def __init__(self, group=None, server_type=None, instance_type=None,
                     environment=None, ami=None, region=None, role=None,
                     keypair=None, availability_zone=None, security_groups=None,
-                    block_devices=None, chef_path=None):
+                    block_devices=None, chef_path=None, couchbase_version=None,
+                    couchbase_username=None, couchbase_password=None):
 
         if server_type is None: server_type = self.SERVER_TYPE
+
+        self.couchbase_version = couchbase_version
+        self.couchbase_username = couchbase_username
+        self.couchbase_password = couchbase_password
 
         super(CacheServer, self).__init__(group, server_type, instance_type,
                                             environment, ami, region, role,
@@ -35,4 +40,38 @@ class CacheServer(Server):
         ]
 
         self.resolve_security_groups()
+
+    def bake(self):
+
+        super(CacheServer, self).bake()
+
+        if not any([self.couchbase_version, self.couchbase_username,
+                        self.couchbase_password]):
+            return
+
+        with self.chef_api:
+
+            if self.couchbase_version:
+                self.chef_node.attributes.set_dotted('couchbase.server.version',
+                                                        self.couchbase_version)
+
+                self.log.info('Set the couchbase.server.version to' \
+                            '{version}'.format(version=self.couchbase_version))
+
+            if self.couchbase_username:
+                self.chef_node.attributes.set_dotted('couchbase.server.username',
+                                                        self.couchbase_username)
+
+                self.log.info('Set the couchbase.server.username to' \
+                            '{username}'.format(username=self.couchbase_username))
+
+            if self.couchbase_password:
+                self.chef_node.attributes.set_dotted('couchbase.server.password',
+                                                        self.couchbase_password)
+
+                self.log.info('Set the couchbase.server.password to' \
+                            '{password}'.format(password=self.couchbase_password))
+
+            self.chef_node.save()
+            self.log.info('Saved the Chef Node configuration')
 
