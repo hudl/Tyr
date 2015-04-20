be12d72f988a6a4b025fb5af4754574d050ea64c
Mihir Singh <git.service@mihirsingh.com>
2015-04-19 23:51:59 -0400

    Mapped the allow-mongo-backup-snapshot and allow-mongo-snapshot-cleanup policies

--- tyr/policies/__init__.py
+++ tyr/policies/__init__.py
@@ -4,5 +4,7 @@ import s3
 policies = {
     'allow-volume-control': ec2.allow_volume_control,
     'allow-upload-to-s3-fulla': s3.allow_upload_to_s3_fulla,
-    'allow-download-scripts-s3-fulla': s3.allow_download_scripts_s3_fulla
+    'allow-download-scripts-s3-fulla': s3.allow_download_scripts_s3_fulla,
+    'allow-mongo-backup-snapshot': ec2.allow_mongo_backup_snapshot,
+    'allow-mongo-snapshot-cleanup': ec2.allow_mongo_snapshot_cleanup
 }
