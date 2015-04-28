c44da6a3774bfa51b70c06a483b47486a80fc4c7
Mihir Singh <git.service@mihirsingh.com>
2015-04-28 11:22:03 -0500

    Created a logger and a timeit function

---
+++ tyr/utilities/replace_couchbase_server.py
@@ -0,0 +1,29 @@
+import time
+import logging
+
+def timeit(method):
+
+    def timed(*args, **kwargs):
+        start = time.time()
+        response = method(*args, **kwargs)
+        end = time.time()
+
+        log.debug('Executed {name} in {elapsed} seconds'.format(
+                                                        name = method.__name__,
+                                                        elapsed = end-start))
+
+        return response
+
+    return timed
+
+log = logging.getLogger('Tyr.Utilities.ReplaceCouchbaseServer')
+if not log.handlers:
+    log.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+            '%(asctime)s [%(name)s] %(levelname)s: %(message)s',
+            datefmt='%H:%M:%S')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
