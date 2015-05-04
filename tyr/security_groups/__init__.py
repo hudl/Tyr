5d98d7a6eaf8f5db57c94286cd8f4cf0b46b4f71
Mihir Singh <git.service@mihirsingh.com>
2015-05-03 21:49:12 -0500

    Defined a security group for cache servers

--- tyr/security_groups/__init__.py
+++ tyr/security_groups/__init__.py
@@ -1,12 +1,36 @@
 security_groups = {
-    't-performancecenter-mongo': {
-        'name': 't-performancecenter-mongo',
+    '.-.+-cache': {
         'rules': [
             {
-                'ip_protocol': 'tcp',
-                'from_port': 27017,
-                'to_port': 27019,
-                'src_group': 't-performancecenter-mongo'
+                'port': '11209 - 11211',
+                'source': [
+                    'thors',
+                    '{env}-{group}-cache'
+                ]
+            },
+            {
+                'port': '21100 - 21199',
+                'source': [
+                    'thors',
+                    '{env}-{group}-cache'
+                ]
+            },
+            {
+                'port': 4369,
+                'source': '{env}-{group}-cache'
+            },
+            {
+                'port': '8091 - 8092',
+                'source': '{env}-{group}-cache'
+            },
+            {
+                'port': '11210 - 11211',
+                'source': [
+                    '216.229.9.101/32',
+                    '24.106.8.210/32',
+                    '74.51.218.230/32',
+                    '75.69.151.220/32'
+                ]
             }
         ]
     }
