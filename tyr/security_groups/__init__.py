3638a83841376ae684fc36ad53293998494d750d
Mihir Singh <git.service@mihirsingh.com>
2015-05-05 13:24:20 -0500

    Defined the management security group

--- tyr/security_groups/__init__.py
+++ tyr/security_groups/__init__.py
@@ -1,5 +1,7 @@
+from tyr.security_groups import management
 from tyr.security_groups import cache
 
 security_groups = {}
 
+security_groups[management.rule] = management.definition
 security_groups[cache.rule] = cache.definition
