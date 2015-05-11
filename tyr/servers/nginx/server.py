2b18215900637a6ffc9ea048c07b9bdfedcbd3ad
Jamie Snell <jgsnell@gmail.com>
2015-05-11 14:20:53 -0500

    add vpc support to nginx spin up

--- tyr/servers/nginx/server.py
+++ tyr/servers/nginx/server.py
@@ -21,14 +21,15 @@ class NginxServer(Server):
     def __init__(self, group=None, server_type=None, instance_type=None,
                     environment=None, ami=None, region=None, role=None,
                     keypair=None, availability_zone=None, security_groups=None,
-                    block_devices=None, chef_path=None):
+                    block_devices=None, chef_path=None, subnet_id=None):
 
         if server_type is None: server_type = self.SERVER_TYPE
 
         super(NginxServer, self).__init__(group, server_type, instance_type,
                                     environment, ami, region, role,
                                     keypair, availability_zone,
-                                    security_groups, chef_path)
+                                    security_groups, block_devices,
+                                    chef_path, subnet_id)
 
     def configure(self):
 
