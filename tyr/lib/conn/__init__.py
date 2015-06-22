bdd4a0c36755a17b70197f91471ef7ef5640764c
Mihir Singh <git.service@mihirsingh.com>
2015-06-22 15:26:58 -0500

    Fixed errors in the documentation for tyr.lib.conn.aws.client

--- tyr/lib/conn/__init__.py
+++ tyr/lib/conn/__init__.py
@@ -7,11 +7,11 @@ import tyr.lib.conn.aws
 def aws_client(service_name, region_name, aws_access_key_id=None,
                aws_secret_access_key=None):
     """
-    Retrieve a client for access a Amazon Web Services service. Clients are
-    maintained in a dictionary, stored with their service name, region name,
-    and the credentials used to access them. If a client with the specified
-    arguments does not already exist, a new client will be created and stored
-    for future use.
+    Retrieve a low level client for accessing an Amazon Web Services service.
+    Clients are maintained in a dictionary, stored with their service name,
+    region name, and the credentials used to establish the connection.
+    If a client with the specified arguments does not already exist, a new
+    client will be created and stored for future use.
 
     :type service_name: string
     :param service_name: The name of the service the client should access
