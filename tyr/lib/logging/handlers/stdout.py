c052fedd4e8a6c6d39b4fddee542470b5b6c520b
Mihir Singh <git.service@mihirsingh.com>
2015-06-23 14:23:27 -0500

    Created a basic printing log handler

---
+++ tyr/lib/logging/handlers/stdout.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+
+def stdout_handler(values):
+    """
+    A handler for Logger which formats and prints events to stdout.
+
+    :type values: dict
+    :param values: A dictionary optionally containing the event and any tags
+    """
+    if 'event' in list(values):
+        template = '{timestamp} [{level}] {path}: {event} [{tags}]'
+    else:
+        template = '{timestamp} [{level}] {path}: [{tags}]'
+
+    params = {}
+
+    params['timestamp'] = str(values.pop('timestamp'))
+    params['level'] = values.pop('level').upper()
+    params['path'] = values.pop('path')
+
+    if 'event' in list(values):
+        params['event'] = values.pop('event')
+
+    params['tags'] = ''
+
+    for key in list(values):
+        params['tags'] = '{tags}\t{key}={value}'.format(
+            tags=params['tags'],
+            key=key,
+            value=values[key])
+
+    params['tags'] = params['tags'].lstrip()
+
+    print(template.format(**params))
