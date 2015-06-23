76fa1665a5246ef83271b2e518649973b3f22b01
Mihir Singh <git.service@mihirsingh.com>
2015-06-23 16:58:57 -0500

    Created a configuration class for managing configurations

---
+++ tyr/lib/configuration/configuration.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import json
+import os.path
+
+
+class Configuration(object):
+    root = '/etc'
+    _kwargs = {}
+    _config = {}
+
+    def __init__(self, path, **kwargs):
+        self.path = path
+        self._kwargs = kwargs
+
+        self._load()
+
+    def reload(self):
+        """
+        Reload the configuration. Useful if any configuration files have
+        been changed since the initial load.
+        """
+
+        self._load()
+
+    def _load(self):
+        """
+        Loads and builds the configuration given a path and any overrides,
+        provided as path and additional keyword arguments in __init__.
+        """
+
+        components = self.path.split('.')
+
+        conf_paths = []
+
+        for index, component in enumerate(components):
+            tree = [self.root]
+            tree.extend(components[0:index+1])
+            tree.append('conf.json')
+
+            conf_paths.append(os.path.join(*tree))
+
+        for conf_path in conf_paths:
+            if os.path.exists(conf_path) and os.path.isfile(conf_path):
+                with open(conf_path, 'r') as f:
+                    config = json.load(f)
+                    self._config.update(config)
+
+        self._config.update(self._kwargs)
+
+    def __getattr__(self, name):
+        """
+        Retrieve a key as an attribute.
+
+        :type name: string
+        :param name: The name of the key to return
+        """
+        return self._config[name]
+
+    def __getitem__(self, key):
+        """
+        Retrieve a key as a dictionary item.
+
+        :type key: string
+        :param key: The key to return
+        """
+        return self._config[key]
+
+    def __setitem__(self, key, value):
+        """
+        Set a key as a dictionary item.
+
+        :type key: string
+        :param key: The key to set
+        :param value: The value to set
+        """
+        self._config[key] = value
+
+    def __delitem__(self, key):
+        """
+        Delete a key from the configuration.
+
+        :type key: string
+        :param key: The key to delete
+        """
+        del self._config[key]
+
+    def __iter__(self):
+        """
+        Retrieve an iterator with the defined keys.
+        """
+        return iter(list(self._config))
+
+    def keys(self):
+        """
+        Retrieve a list of the defined keys.
+        """
+        return list(self._config)
