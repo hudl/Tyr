d610e5ee5fe311ab4252a73f5a930050d59cc351
J. Malcolm Preston <malcolm.preston@hudl.com>
2015-07-09 14:15:33 -0400

    adding elasticsearch stuff

---
+++ tyr/servers/elasticsearch/server.py
@@ -0,0 +1,29 @@
+from tyr.servers.server import Server
+
+
+class ElasticsearchServer(Server):
+
+    SERVER_TYPE = 'elasticsearch'
+
+    CHEF_RUNLIST = ['role[RoleElasticsearch]']
+
+    IAM_ROLE_POLICIES = [
+        'allow-describe-tags',
+        'allow-describe-instances',
+        'allow-volume-control',
+        'allow-update-route53-{environment}'
+    ]
+
+    def __init__(self, group=None, server_type=None, instance_type=None,
+                    environment=None, ami=None, region=None, role=None,
+                    keypair=None, availability_zone=None, security_groups=None,
+                    block_devices=None, chef_path=None, subnet_id=None,
+                    dns_zones=None):
+
+        if server_type is None: server_type = self.SERVER_TYPE
+
+        super(ElasticsearchServer, self).__init__(group, server_type, instance_type,
+                                             environment, ami, region, role,
+                                             keypair, availability_zone,
+                                             security_groups, block_devices,
+                                             chef_path, subnet_id, dns_zones)
