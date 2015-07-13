fd7844cea6d831350fce85891ff5216a079c65a2
Mihir Singh <git.service@mihirsingh.com>
2015-07-13 14:14:56 -0700

    Added support for provisioning Solr Slave Nodes

--- tyr/servers/solr/__init__.py
+++ tyr/servers/solr/__init__.py
@@ -1,1 +1,2 @@
 from tyr.servers.solr.master import SolrMasterNode
+from tyr.servers.solr.slave import SolrSlaveNode
---
+++ tyr/servers/solr/slave.py
@@ -0,0 +1,52 @@
+from tyr.servers.server import Server
+import sys
+
+
+class SolrSlaveNode(Server):
+
+    SERVER_TYPE = 'solr'
+    CHEF_RUNLIST = ['role[RoleSolr]']
+    IAM_ROLE_POLICIES = ['allow-volume-control']
+
+    def __init__(self, group=None, server_type=None, instance_type=None,
+                 environment=None, ami=None, region=None, role=None,
+                 keypair=None, availability_zone=None, security_groups=None,
+                 block_devices=None, chef_path=None, subnet_id=None,
+                 dns_zones=None, master=None):
+
+        self.master = master
+
+        if server_type is None: server_type = self.SERVER_TYPE
+
+        super(SolrSlaveNode, self).__init__(group, server_type, instance_type,
+                                            environment, ami, region, role,
+                                            keypair, availability_zone,
+                                            security_groups, block_devices,
+                                            chef_path, subnet_id, dns_zones)
+
+    def configure(self):
+
+        super(SolrSlaveNode, self).configure()
+
+        if self.master is None:
+            self.log.critical('The solr master is not defined')
+            sys.exit(1)
+
+    def bake(self):
+
+        super(SolrSlaveNode, self).bake()
+
+        with self.chef_api:
+            self.chef_node.attributes.set_dotted('solr.is_slave', True)
+            self.log.info('Set solr.is_slave to True')
+
+            self.chef_node.attributes.set_dotted('solr.is_master', False)
+            self.log.info('Set solr.is_master to False')
+
+            self.chef_node.attributes.set_dotted('solr.master_host',
+                                                 self.master)
+            self.log.info('Set solr.master_host to {master}'.format(
+                          master=self.master))
+
+            self.chef_node.save()
+            self.log.info('Saved the Chef Node configuration')
