25fd8b4dda1871d9297a1117610dd0189e1c5e40
J. Malcolm Preston <malcolm.preston@hudl.com>
2015-07-15 10:36:01 -0400

    fixing formatting

--- tyr/servers/elasticsearch/server.py
+++ tyr/servers/elasticsearch/server.py
@@ -15,15 +15,16 @@ class ElasticsearchServer(Server):
     ]
 
     def __init__(self, group=None, server_type=None, instance_type=None,
-                    environment=None, ami=None, region=None, role=None,
-                    keypair=None, availability_zone=None, security_groups=None,
-                    block_devices=None, chef_path=None, subnet_id=None,
-                    dns_zones=None):
+                environment=None, ami=None, region=None, role=None,
+                keypair=None, availability_zone=None, security_groups=None,
+                block_devices=None, chef_path=None, subnet_id=None,
+                dns_zones=None):
 
         if server_type is None: server_type = self.SERVER_TYPE
 
-        super(ElasticsearchServer, self).__init__(group, server_type, instance_type,
-                                             environment, ami, region, role,
-                                             keypair, availability_zone,
-                                             security_groups, block_devices,
-                                             chef_path, subnet_id, dns_zones)
+        super(ElasticsearchServer, self).__init__(group, server_type,
+                                                instance_type, environment,
+                                                ami, region, role, keypair,
+                                                availability_zone, chef_path,
+                                                security_groups, block_devices,
+                                                subnet_id, dns_zones)
