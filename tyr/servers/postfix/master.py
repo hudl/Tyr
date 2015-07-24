cdab291279193671c7a18c4c7ac49961d39d89d9
Steve Schmidt <steve.schmidt@hudl.com>
2015-07-24 11:21:20 -0400

    Add some feature enhancements to replacing Postfix Master
      - Determine the EIP associated with the postfix master you're
        rebuilding.
      - Logic around handling the mail name - make sure it's a FQDN unless
        the user passed it in that way.
      - In order to associate an EIP with an instance, we get the allocation
        ID for that EIP. Add functionality around obtaining the allocation
        ID.
      - Assigning the EIP must occur after the instance is online, so
        override the autorun function from the Server superclass to order
        this in the correct manner.

--- tyr/servers/postfix/master.py
+++ tyr/servers/postfix/master.py
@@ -6,8 +6,9 @@ import re
 
 class PostfixMaster(Server):
 
-    SERVER_TYPE = 'postfix'
+    SERVER_TYPE  = 'postfix'
     CHEF_RUNLIST = ['role[RolePostfix]']
+    ELASTIC_IP   = ''
 
     #IAM_ROLE_POLICIES = [ ]
 
@@ -18,11 +19,6 @@ class PostfixMaster(Server):
                  chef_path=None, subnet_id=None, dns_zones=None,
                  mail_name=None):
 
-        if re.match('^.+hudl\.com$',mail_name) is None:
-            self.mail_name = mail_name + '.hudl.com'
-        else:
-            self.mail_name = mail_name
-
         if server_type is None:
             server_type = self.SERVER_TYPE
 
@@ -32,19 +28,32 @@ class PostfixMaster(Server):
                                         security_groups, block_devices,
                                         chef_path, subnet_id, dns_zones)
 
+        if mail_name is not None:
+            if re.match('^.+hudl\.com$',mail_name) is None:
+                self.mail_name = mail_name + '.hudl.com'
+            else:
+                self.mail_name = mail_name
+        else:
+            raise Exception('Must pass existing mail server name!')
+            sys.exit(1)
+
     def configure(self):
         super(PostfixMaster, self).configure()
-
+        """
+        1.) Verify that the mail server name exists in DNS and there's an EIP
+        associated with it.
+        - Cannot assign the IP until there's an instance ID.
+        """
         #Check for mail server name in AWS and to see if it has an EIP.
         if self.mail_name:
-            route53_conn = self.establish_route53_connection()
+            route53_conn = self.route53
             elastic_ip   = self.check_mail_server(route53_conn)
+            self.ELASTIC_IP = elastic_ip
             self.log.info('Found mail server!')
-            # Assign the elastic ip to the instance.
-            self.assign_eip(elastic_ip)
         else:
             self.log.critical('Must specify a mail server configured in route53!')
             sys.exit(1)
+
 
     # Params: Route53 connection object from boto
     # Return: EIP
@@ -54,7 +63,7 @@ class PostfixMaster(Server):
         supplied mail server name.
         """
         hosted_zone    = 'hudl.com'
-        zone_obj       = conn.get_zone(hosted_zone)
+        zone_obj       = route53_conn.get_zone(hosted_zone)
         try:
             a_record = zone_obj.get_a(self.mail_name)
             if a_record:
@@ -64,29 +73,59 @@ class PostfixMaster(Server):
                 self.log.info('Using Elastic IP {elastic_ip}...'.format(
                                                     elastic_ip=elastic_ip))
             else:
-                raise 'Unable to find mail server in route53!'
+                raise Exception('Unable to find mail server {mail_name} in '
+                                'route53!'.format(mail_name=self.mail_name))
         except Exception, e:
             self.log.critical(str(e))
             sys.exit(1)
 
         return elastic_ip
 
-    def assign_eip(self,elastic_ip):
-        ec2_conn    = self.establish_ec2_connection()
+    def assign_eip(self):
+        ec2_conn    = self.ec2
         instance_id = self.instance.id
+
         try:
-            result = ec2_conn.associate_address(instance_id,elastic_ip,None,None,
-                                                None,True,True)
+            alloc_id = self.get_alloc_id(ec2_conn)
+            if alloc_id is None:
+                raise Exception('The EIP associated with the DNS name for '
+                                '{mail_name} does not have an allocation ID '
+                                'associated with it. This issue can be resolved '
+                                'by migrating the EIP from EC2-Classic to '
+                                'EC2-VPC.'.format(mail_name=self.mail_name))
+
+            result = ec2_conn.associate_address(instance_id=instance_id,
+                                                public_ip=None,
+                                                allocation_id=alloc_id,
+                                                network_interface_id=None,
+                                                private_ip_address=None,
+                                                allow_reassociation=True,
+                                                dry_run=True)
             if result:
                 self.log.info('Successfully assigned EIP: {elastic_ip} '
                               'to {mail_name}'.format(elastic_ip=elastic_ip,
                                                       mail_name=self.mail_name))
             else:
-                raise 'Unable to assign EIP! Exiting...'
+                raise Exception('Unable to assign EIP: {eip}! '
+                                'Exiting...'.format(eip=elastic_ip))
         except Exception,e:
-            self.log.critical(str(e))
-            sys.exit(1)
+            self.log.error(str(e))
 
+    def get_alloc_id(self,ec2_conn):
+        try:
+            eip_addresses = ec2_conn.get_all_addresses()
+
+            for address_obj in eip_addresses:
+                address_str = str(address_obj)
+                ip = address_str.split(':')[1]
+                if ip == self.ELASTIC_IP:
+                    alloc_id = address_obj.allocation_id
+                    break
+
+        except Exception,e:
+            self.log.error(str(e))
+
+        return alloc_id
 
     def bake(self):
         super(PostfixMaster, self).bake()
@@ -99,3 +138,9 @@ class PostfixMaster(Server):
 
             self.chef_node.save()
             self.log.info('Saved the Chef node configuration')
+
+    def autorun(self):
+        super(PostfixMaster, self).autorun()
+
+        if self.baked():
+           self.assign_eip()
