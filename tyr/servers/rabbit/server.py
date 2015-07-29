1286dbe3e906b642cd3a2daa767b3a585c6014d3
Jamie Snell <jamie.snell@hudl.com>
2015-07-29 17:38:22 -0500

    added spaces after comma and indented properly

--- tyr/servers/rabbit/server.py
+++ tyr/servers/rabbit/server.py
@@ -15,10 +15,11 @@ class RabbitMQServer(Server):
     ]
 
     def __init__(self, group=None, server_type=None, instance_type=None,
-                    environment=None, ami=None, region=None, role=None,
-                    keypair=None, availability_zone=None, security_groups=None,
-                    block_devices=None, chef_path=None, subnet_id=None,
-                    dns_zones=None,vol_iops=500,vol_size=100,rabbit_user=None,rabbit_pass=None):
+                 environment=None, ami=None, region=None, role=None,
+                 keypair=None, availability_zone=None, security_groups=None,
+                 block_devices=None, chef_path=None, subnet_id=None,
+                 dns_zones=None, vol_iops=500, vol_size=100,
+                 rabbit_user=None, rabbit_pass=None):
 
         if server_type is None: server_type = self.SERVER_TYPE
 
