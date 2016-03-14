8c607e786e0b942c3f5b6822272efbc31117125c
Mihir Singh <git.service@mihirsingh.com>
2016-03-14 10:46:46 -0400

    Updated setup.py to use find_packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tyr',
     version='0.0.1',
     author='Mihir Singh (@citruspi)',
     author_email='mihir.singh@hudl.com',
-    packages=['tyr'],
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
