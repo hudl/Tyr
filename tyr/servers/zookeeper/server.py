9b8018242fd92dbe8c22d6dcf2311fc937802a69
Alex DeBrie <alexdebrie1@gmail.com>
2016-05-20 12:59:38 +0000

    Add Zookeeper server type

---
+++ tyr/servers/zookeeper/server.py
@@ -0,0 +1,29 @@
+from tyr.servers.server import Server
+
+
+class ZookeeperServer(Server):
+
+    SERVER_TYPE = 'zookeeper'
+
+    CHEF_RUNLIST = ['role[RoleZookeeper]']
+
+    IAM_ROLE_POLICIES = [
+        'allow-describe-instances',
+        'allow-describe-tags',
+        'allow-volume-control'
+    ]
+
+    def __init__(self, group=None, server_type=None, instance_type=None,
+                 environment=None, ami=None, region=None, role=None,
+                 keypair=None, availability_zone=None, security_groups=None,
+                 block_devices=None, chef_path=None, subnet_id=None,
+                 dns_zones=None):
+
+        if server_type is None:
+            server_type = self.SERVER_TYPE
+
+        super(ZookeeperServer, self).__init__(group, server_type, instance_type,
+                                              environment, ami, region, role,
+                                              keypair, availability_zone,
+                                              security_groups, block_devices,
+                                              chef_path, subnet_id, dns_zones)
