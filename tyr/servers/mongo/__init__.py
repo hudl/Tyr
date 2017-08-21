439ba86b1b1879d2b64ec4e633814f1276473f2a
Mihir Singh <git.service@mihirsingh.com>
2017-08-21 14:42:47 -0400

    Removed reference to MongoDataWarehousingNode

--- tyr/servers/mongo/__init__.py
+++ tyr/servers/mongo/__init__.py
@@ -1,5 +1,4 @@
 from data import MongoDataNode
 from arbiter import MongoArbiterNode
-from data_warehousing import MongoDataWarehousingNode
 from config import MongoConfigNode
 from zuun import ZuunConfig
