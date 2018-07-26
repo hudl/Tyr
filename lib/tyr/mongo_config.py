501a481baf6ad1b316dde1b8e1fea1ffa396afcf
Mihir Singh <git.service@mihirsingh.com>
2018-07-26 15:39:55 +0000

    Implemented MongoDB data nodes, routers, and config servers

---
+++ lib/tyr/mongo_config.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from superkwargs import superkwarg, kwarg, exceptions, typing
+from infrakit.mongodb.rs_mods import REPLICA_SET_MODS
+from infrakit.tyr.mongo_node import MongoNode
+
+
+class MongoConfigNode(MongoNode):
+    
+    @kwarg('subrole', default='conf')
+    @kwarg('replicated', default=lambda kw: True if kw['group'] == 'highlights' else False)
+    @kwarg('data_volume_size', default=15)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        if self.replicated:
+            self.replica_set_name = REPLICA_SET_MODS[self.group]['config'](self)
+
+
+    @property
+    def tags(self):
+        t = super().tags
+
+        if self.replicated:
+            t.update({
+                'ReplicaSet': self.replica_set_name
+            })
+
+        return t
+
+    @property
+    def chef_node_attributes(self):
+        a = super().chef_node_attributes
+
+        if self.replicated:
+            a.update({
+                'mongodb': {
+                    'replicaset_name': self.replica_set_name
+                },
+                'zuun': {
+                    'deployment': self.zuun_deployment,
+                    'role': 'config',
+                    'replica_set': self.replica_set_name
+                }
+            })
+        else:
+            a.update({
+                'zuun': {
+                    'deployment': self.zuun_deployment,
+                    'role': 'conf'
+                }
+            })
+
+        return a
---
+++ lib/tyr/mongo_router.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from superkwargs import superkwarg, kwarg, exceptions, typing
+from infrakit.mongodb.rs_mods import REPLICA_SET_MODS
+from infrakit.tyr.mongo_node import MongoNode
+
+
+class MongoRouterNode(MongoNode):
+    
+    @kwarg('subrole', default='router')
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    
+    @property
+    def chef_node_attributes(self):
+        a = super().chef_node_attributes
+
+        a.update({
+            'zuun': {
+                'deployment': self.zuun_deployment,
+                'role': 'router'
+            }
+        })
+
+        return a
